5fb07e926a3e5d475973133bb6528e3e92ef169f
Maxim <maxim.makarov14@gmail.com>
2021-05-26 11:43:46 +0300

    Generated lib components

---
+++ src/app/components/Lib/Button/Button.js
@@ -0,0 +1,7 @@
+import { Button as AntButton } from 'antd-styled'
+import styled from 'styled-components'
+// import { variant } from 'styled-system'
+
+const Button = styled(AntButton)
+
+export default Button
